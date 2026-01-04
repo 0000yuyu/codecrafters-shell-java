37fd1bdfd375f27aa021cdec2cb0252a996cc693
yujin <yujin09219@naver.com>
2026-01-04 04:18:07 +0000

    second : command error

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
+        Scanner sc = new Scanner(System.in);
         System.out.print("$ ");
+        String command = sc.next();
+        System.out.println(command + ": command not found");
+        sc.close();
     }
 }
